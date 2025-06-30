a3bf655f8662313a36497353a14dd5aaa63b562e
Aman Sharma <156844703+AmanSharma39@users.noreply.github.com>
2025-06-30 10:43:01 +0530

    Update main.tf

--- Terraform/environments/dev/main.tf
+++ Terraform/environments/dev/main.tf
@@ -2,19 +2,14 @@ provider "aws" {
   region = "eu-west-1"
 }
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    hashicorp = {
-      source  = "hashicorp/hashicorp"  # Note: HashiCorp provider example; typically you add specific providers like vault, consul, etc.
-      version = "~> 0.1"
+      source = "hashicorp/aws"
+      version = "6.0.0"
     }
   }
 }
+
 # module "secrets" {
 #   source      = "../../modules/secrets"
 #   env_name    = "dev"
